059235c7d396babf9b44806a4283cac614b9377c
Timothy RYAN <ryaninlux@gmail.com>
2022-08-25 15:10:23 -0700

    Create all models

---
+++ PierresTreats/Models/Treat.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace PierresTreats.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.JoinEntities = new HashSet<TreatFlavor>();
+    }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public string Price { get; set; }
+
+    public virtual ApplicationUser User { get; set; }
+
+    public virtual ICollection<RecipeCategory> JoinEntities { get;}
+
+  }
+}
